354c25f7cde2cdc6781a780bb03656d17c38d56f
kowsi123 <kowsalayadevi99@gmail.com>
2021-05-10 19:42:08 +0530

    commit two

--- src/main/java/com/cognizant/Main.java
+++ src/main/java/com/cognizant/Main.java
@@ -1,7 +1,11 @@
 package com.cognizant;
 
+import com.cognizant.model.Person;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		Person person=new Person();
+		System.out.println("hello "+person.getName()+" welcome");
 	}
 }
---
+++ src/main/java/com/cognizant/model/Person.java
@@ -0,0 +1,14 @@
+package com.cognizant.model;
+
+public class Person {
+	String name="kowsi";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
