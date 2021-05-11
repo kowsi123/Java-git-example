637abf069ef93070fefe20da35dbf6c736dd3fe9
RamSubash <jaisubashram@gmail.com>
2021-05-11 16:17:03 +0530

    b1

--- src/main/java/com/cognizant/Main.java
+++ src/main/java/com/cognizant/Main.java
@@ -1,12 +1,15 @@
 package com.cognizant;
 
+import com.cognizant.model.Model;
 import com.cognizant.model.Person;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		Person person=new Person();
+		Model model =new Model();
 		System.out.println("hello "+person.getName()+" welcome");
-		System.out.println("I am Kowsi from Erode");
+		System.out.println(model.getMessage());
+		
 	}
 }
